5fb55e3fb7211188087bd15f45ab02198e800a84
VladimirMedv <167883925+VladimirMedv@users.noreply.github.com>
2024-08-19 15:29:26 -0300

    the main functionality has been implemented

---
+++ components/SearchInput.jsx
@@ -0,0 +1,36 @@
+import { View, TextInput, Image, TouchableOpacity, Alert } from "react-native";
+import React, { useState } from "react";
+import { router, usePathname } from "expo-router";
+
+import { icons } from "../constants";
+
+const SearchInput = ({ initialQuery }) => {
+  const pathname = usePathname();
+  const [query, setQuery] = useState(initialQuery || "");
+
+  return (
+    <View className="flex-row items-center space-x-4 w-full h-16 px-4 bg-black-100 rounded-2xl border-2 border-black-200 focus:border-secondary">
+      <TextInput
+        className="text-base mt-0.5 text-white flex-1 font-pregular"
+        value={query}
+        placeholder="Search a video topic"
+        placeholderTextColor="#CDCDE0"
+        onChangeText={(e) => setQuery(e)}
+      />
+
+      <TouchableOpacity
+        onPress={() => {
+          if (query === "")
+            return Alert.alert("Missing query", "Please enter a search query");
+
+          if (pathname.startsWith("/search")) router.setParams({ query });
+          else router.push(`/search/${query}`);
+        }}
+      >
+        <Image source={icons.search} resizeMode="contain" className="w-5 h-5" />
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+export default SearchInput;
